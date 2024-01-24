74398bd58a44c8805b384bb62c2738c798813f7c
chaitanya <chaitanyaarora3@gmail.com>
2024-01-25 00:44:15 +0530

    marble: update to hyper os 1.0.2.0.UMRMIXM

--- device.mk
+++ device.mk
@@ -667,6 +667,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils-shim
 
 # USB
 PRODUCT_PACKAGES += \
