cd47475ea3ce3a2712195df4ed8db66755fe73c5
Maulana Riaddy <maulriaddy@gmail.com>
2024-01-26 11:11:22 +0700

    marble: Bringup apollo os

--- apollo_marble.mk
+++ apollo_marble.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Apollo OS stuff.
+$(call inherit-product, vendor/apollo/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Build stuff.
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_FACE_UNLOCK_OPTOUT := true
-TARGET_SUPPORTS_QUICK_TAP := true
+# Build stuff
+TARGET_DISABLE_EPPE := true
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_FACE_UNLOCK_OPTOUT := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
@@ -26,7 +26,13 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_marble
+PRODUCT_NAME := apollo_marble
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Gapps
+WITH_GAPPS := true
